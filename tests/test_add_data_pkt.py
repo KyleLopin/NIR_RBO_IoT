8dace1d78933db73f8ebe6b9570effcc574c0dd9
Zanith <kvl6@case.edu>
2022-11-18 10:13:08 +0700

    pass computer, seminar ignroe this update

--- tests/test_add_data_pkt.py
+++ tests/test_add_data_pkt.py
@@ -6,9 +6,18 @@
 
 __author__ = "Kyle Vitautus Lopin"
 
+import unittest
 
-DATA_PKT1 = {"time": "09:55:22", "date": "2022-11-18", "packet_id": 1,
-             "device": "position 2", "mode": "live", "OryConc": -20139,
-             "CPUTemp": "48.31", "SensorTemp": 0}'
+DATA_PKT1 = b'{"time": "09:55:22", "date": "2022-11-18", "packet_id": 1, ' \
+            b'"device": "position 2", "mode": "live", "OryConc": -20139, ' \
+            b'"CPUTemp": "48.31", "SensorTemp": 0}'
+DATA_PKT2 = b'{"time": "10:05:13", "date": "2022-11-18", "packet_id": 3, ' \
+            b'"device": "position 2", "mode": "live", "OryConc": -20602, ' \
+            b'"CPUTemp": "48.31", "SensorTemp": "41.79"}'
+DATA_PKT3 = b'{"time": "10:06:13", "date": "2022-11-18", "packet_id": 4, ' \
+            b'"device": "position 2", "mode": "live", "OryConc": -20648, ' \
+            b'"CPUTemp": "47.24", "SensorTemp": 0}'
 
 
+class TestAddDataPacket(unittest.TestCase):
+    def
